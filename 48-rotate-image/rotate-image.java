11d495a3e3b81eb06fc8969efb90e80e3fcf6df6
KUMBOJI SAI VAMSHI <24211a05w2@gmail.com>
2026-08-12 19:30:47 +0530

    Time: 11 ms (1.35%) | Memory: 44.1 MB (16.60%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,17 +1,4 @@
 class Solution {
-    public void reverse(int[][] matrix,int n){
-        for(int i=0;i<n;i++){
-            int p1=0,p2=n-1;
-            while(p1<=p2){
-                int temp = matrix[i][p1];
-                matrix[i][p1] = matrix[i][p2];
-                matrix[i][p2] = temp;
-                p1++;
-                p2--;
-            }
-        }
-        printArray(matrix,matrix.length);
-    }
 
     public void printArray(int[][] matrix,int n){
         for(int i=0;i<n;i++){
@@ -29,6 +16,16 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
-        reverse(matrix,matrix.length);
+        for(int i=0;i<n;i++){
+            int p1=0,p2=n-1;
+            while(p1<=p2){
+                int temp = matrix[i][p1];
+                matrix[i][p1] = matrix[i][p2];
+                matrix[i][p2] = temp;
+                p1++;
+                p2--;
+            }
+        }
+        printArray(matrix,matrix.length);
     }
 }
