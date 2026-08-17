3f2025735a2df71662ce6730b23286f1d5e9a00f
KUMBOJI SAI VAMSHI <24211a05w2@gmail.com>
2026-08-17 17:51:25 +0530

    Time: 24 ms (76.09%) | Memory: 48.6 MB (79.67%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -3,14 +3,20 @@ class Solution {
         int n = nums.length;
         int count = 0;
         int sum = 0;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        hm.put(0,1);
         for(int i=0;i<n;i++){
-            sum=0;
-            for(int j=i;j<n;j++){
-                sum += nums[j];
-                if(sum == k){
-                    count++;
-                }
-            }
+            sum += nums[i];
+            int remove = sum - k;
+            count += hm.getOrDefault(remove,0);
+            hm.put(sum,hm.getOrDefault(sum,0) + 1);
+            // sum=0;
+            // for(int j=i;j<n;j++){
+            //     sum += nums[j];
+            //     if(sum == k){
+            //         count++;
+            //     }
+            // }
         }
         return count;
     }
