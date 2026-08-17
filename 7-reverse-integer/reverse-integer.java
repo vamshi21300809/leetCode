b0f5c9560543b89b427c6d65abb6d7b614f889e7
KUMBOJI SAI VAMSHI <24211a05w2@gmail.com>
2026-08-17 18:36:54 +0530

    Time: 1 ms (99.98%) | Memory: 42.5 MB (80.66%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        while(x != 0){
+            int r = x % 10;
+            if(res > Integer.MAX_VALUE / 10 || res < Integer.MIN_VALUE / 10){
+                return 0;
+            }
+            res = res * 10 + r;
+            x /= 10;
+        }
+        return res;
+    }
+}
