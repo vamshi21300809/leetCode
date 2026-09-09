a8b68a4bbcac277d79156b2678533890efa5efb6
KUMBOJI SAI VAMSHI <24211a05w2@gmail.com>
2026-09-09 18:54:15 +0530

    Time: 10 ms (21.14%) | Memory: 49.1 MB (42.38%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,5 +1,8 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        if(intervals.length <= 1){
+            return intervals;
+        }
         List<int[]> ans = new ArrayList<>();
         Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
         int n = intervals.length;
