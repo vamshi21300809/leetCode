17059983b1fa96be95069fe36bea5e770920f4e0
KUMBOJI SAI VAMSHI <24211a05w2@gmail.com>
2026-09-10 18:42:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (53.23%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -3,10 +3,13 @@ class Solution {
         int n = nums.length;
         int max = 1,min = 1;
         int ans = nums[0];
-        for(int i=0;i<n;i++){
-            int temp = max * nums[i];
-            max = Math.max(temp,Math.max(min * nums[i],nums[i]));
-            min = Math.min(temp,Math.min(min * nums[i],nums[i]));
+        for(int i: nums){
+            ans = Math.max(ans,i);
+        }
+        for(int i : nums){
+            int temp = max * i;
+            max = Math.max(temp,Math.max(min * i,i));
+            min = Math.min(temp,Math.min(min * i,i));
 
             ans = Math.max(max,ans);
         }
